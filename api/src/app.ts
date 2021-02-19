cbafd3e5d040ed3f7e59876d6d18ad969897f843
amihan00 <amihan00@fesb.hr>
2021-02-19 17:35:06 +0100

    Created API and client

---
+++ api/src/app.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
